ca96aef7cab60556ccd5dd37364bc478eddd8b1e
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-06-05 07:02:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(name, email) {
+  const newUser = {
+    name,
+    email,
+  };
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(newUser),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      alert('stop stop stop');
+      console.log(error.message);
+    });
+}
+
+// submitData('test', 'test.@test.com');
